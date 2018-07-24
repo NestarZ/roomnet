61dcee0c8e9409ccc94c7f115e7fd1075d3a3abc
Elias Rhouzlane <elias.rhouzlane@gmail.com>
2018-07-24 14:43:44 +0200

    Create setup.sh

---
+++ setup.sh
@@ -0,0 +1,10 @@
+mkdir data && cd data
+wget http://lsun.cs.princeton.edu/challenge/2015/roomlayout/data/image.zip
+wget http://lsun.cs.princeton.edu/challenge/2015/roomlayout/data/training.mat
+wget http://lsun.cs.princeton.edu/challenge/2015/roomlayout/data/validation.mat
+wget http://lsun.cs.princeton.edu/challenge/2015/roomlayout/data/testing.mat
+unzip image.zip
+rm -rf image.zip
+
+python prepare_data.py
+python2 main.py --train 0 --net rcnn --out_path ../OUTPUT/
